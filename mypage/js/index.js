007eb07b628fce932fd237e06f41af5aa2625e63
cicada23 <18330636183@163.com>
2017-07-04 21:30:08 +0800

    myblog

--- mypage/js/index.js
+++ mypage/js/index.js
@@ -1,140 +1,130 @@
-//构建一个函数，movePage
-// 参数：滑动的方向
-//功能：根据滑动的方向，显示页面切换的效果
-//需求:1.每次切换页面，页面中的图片重新执行入场动画
-//     2.有滑动边界，到达边界页后相应方向的滑动失效
-//     3.每次上下滑动，如果存在page-n-1/page-n-2，总是显示page-n-1
 
-//实现思路:1.单页展示：本项目的特点是单页展示，每一页都与容器等大，屏幕只展示一个页面
-//          根据本项目的页面结构，实现这一功能方法是，将不展示的页面设为display：none；
-//              1.1 但在切换时出场页面和入场页面同时得到展示，这就要确保此时两个页面的
-//               都为display：block；
-
-
-//在进入逻辑前nowPage就是page-1-1
-var nowPage = {col:1,row:1};
-var lastPage = {col:0,row:0};
-var dir = {up:"up",right:"right",down:"down",left:"left"}
+/**
+ * Created by zhiyongYan on 2017/3/6.
+ */
+$(function () {
+	//初始化坐标变量
+	var last = {row:0,col:0};
+	var now = {row:1,col:1};
+	//初始化四个方向变量
+	var direction = {up:1,right:2,down:3,left:4};
+	//初始化是否移动变量
+	var isMoving = false;
 
 
-//滑动的响应逻辑
-//由于在滑动后nowPage会变成lastPage，且会根据lastPage决定是否响应换页
-//条件限制，页面第一次之后只有触发swipeUp时才会触发movePage（）；
-//上滑逻辑
-$(document).swipeUp(function () {
-    lastPage.col = nowPage.col;
-    lastPage.row = nowPage.row;
-    if (lastPage.col === 5 ){
-        return;
-    }else{
-        movePage(dir.up)
-    }
+	//向上滑动
+	$(document).swipeUp(function () {
+		if(isMoving){
+			return;
+		}
+		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
+		last.row = now.row;
+		last.col = now.col;
+		//判断滑动的条件  滑动所需参数的设置
+		if(last.col!=5){
+			now.row = 1;
+			now.col = last.col + 1;
+			pageMove(direction.up);
+		}
+	});
+
+	//向下滑动
+	$(document).swipeDown(function () {
+		if(isMoving){
+			return;
+		}
+		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
+		last.row = now.row;
+		last.col = now.col;
+		//判断滑动的条件  滑动所需参数的设置
+		if(last.col!=1){
+			now.row = 1;
+			now.col = last.col - 1;
+			pageMove(direction.down);
+		}
+	});
+
+	//向左滑动
+	$(document).swipeLeft(function () {
+		if(isMoving){
+			return;
+		}
+		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
+		last.row = now.row;
+		last.col = now.col;
+		//判断滑动的条件  滑动所需参数的设置
+		if(last.col>1&&last.col<5&&last.row==1){
+			now.row = last.row + 1;
+			pageMove(direction.left);
+		}
+	});
+
+	//向右滑动
+	$(document).swipeRight(function () {
+		if(isMoving){
+			return;
+		}
+		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
+		last.row = now.row;
+		last.col = now.col;
+		//判断滑动的条件  滑动所需参数的设置
+		if(last.col>1&&last.col<5&&last.row==2){
+			now.row = last.row - 1;
+			pageMove(direction.right);
+		}
+	});
+	//定义move函数
+	function pageMove(dir) {
+		//初始化作用的两个页面
+		var lastPage = '.page-' + last.col + '-' + last.row;
+		var nowPage = '.page-' + now.col + '-' + now.row;
+
+		//初始化页面类
+		var outClass = '';
+		var inClass = '';
+		//匹配方向
+		switch (dir){
+			case direction.up:
+				outClass = 'pt-page-moveToTop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case direction.right:
+				outClass = 'pt-page-moveToRight';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case direction.down:
+				outClass = 'pt-page-moveToBottom';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case direction.left:
+				outClass = 'pt-page-moveToLeft';
+				inClass = 'pt-page-moveFromRight';
+				break;
+		}
+		//为页面添加动画开始的类
+		$(nowPage).removeClass('hide');
+
+		$(lastPage).addClass(outClass);
+		$(nowPage).addClass(inClass);
+		isMoving = true;
+		//处理动画完成时页面的类
+		setTimeout(function () {
+			$(lastPage).addClass('hide');
+			$(lastPage).removeClass(outClass);
+			// $(lastPage).removeClass('page-current');
+			$(lastPage).find('img').addClass('hide');
+
+
+			//猜测给nowpage提升层级是考虑到，所有的page因为定位的缘故都是重叠的
+			//所以为了确保nowpage的显示吧它的层级调高
+			//但这种考虑有些多余，只需要确保其他page均是hide即可
+			//如果没有这个
+			// $(nowPage).addClass('page-current');
+			$(nowPage).removeClass(inClass);
+			$(nowPage).find('img').removeClass('hide');
+			isMoving = false;
+		},600);
+	}
 });
-
-//向下的逻辑
-$(document).swipeDown(function () {
-    lastPage.col = nowPage.col;
-    lastPage.row = nowPage.row;
-    if (lastPage.col === 1) {
-        return;
-    } else {
-        movePage(dir.down)
-    }
-});
-//向左逻辑,在这个逻辑里要考虑 第一行和最后一行都是单页
-$(document).swipeLeft(function () {
-    lastPage.col = nowPage.col;
-    lastPage.row = nowPage.row;
-    if (lastPage.row === 2 ||lastPage.col === 1||lastPage.col ===5) {
-        return;
-    } else {
-        movePage(dir.left)
-    }
-});
-
-//向右逻辑
-$(document).swipeRight(function () {
-    lastPage.col = nowPage.col;
-    lastPage.row = nowPage.row;
-    if (lastPage.row === 1) {
-        return;
-    } else {
-        movePage(dir.right)
-    }
-});
-
-var isMoving =false;
-function movePage(dirFlag) {
-    //每次进入逻辑前判断是否在执行页面切换，如果已经在执行，直接返回不进入逻辑
-    if(isMoving){
-        return;
-    }
-
-
-    var outClass = "";
-    var inClass = "";
-    //根据滑动方向确定入场页 --nowPage
-    switch (dirFlag){
-        case dir.up:
-            nowPage.col = nowPage.col+1;
-            nowPage.row = 1;
-            outClass = "moveToTop";
-            inClass = "moveFormBottom";
-             break;
-        case dir.right:
-            nowPage.col = lastPage.col;
-            nowPage.row = lastPage.row-1;
-            outClass = "moveToRight";
-            inClass = "moveFormLeft";
-            break;
-        case dir.down:
-            nowPage.col = nowPage.col-1;
-            nowPage.row = 1;
-            console.log(nowPage.col);
-            console.log(nowPage.row);
-            outClass = "moveToBottom";
-            inClass = "moveFormTop";
-            break;
-        case dir.left:
-            nowPage.col = lastPage.col;
-            nowPage.row = lastPage.row+1;
-            outClass = "moveToLeft";
-            inClass = "moveFormRight";
-            break;
-    }
-    console.log(nowPage.col,nowPage.row)
-    var last =".page-"+lastPage.col+"-"+lastPage.row;
-    var now =".page-"+nowPage.col+"-"+nowPage.row;
-
-    $(last).addClass(outClass);
-    //在即将切换之前将isMoving置为true
-    isMoving = true;
-    //在执行进场动画之前需要让他现行
-    //让即将进场的页的图片隐藏
-    $(now).removeClass("hide");
-    // $(now).find("img").addClass("hide");
-    $(now).addClass(inClass);
-
-
-    setTimeout(function () {
-        //在页面动画结束之后，图片开始“加载”
-        $(now).find("img").removeClass("hide");
-        //出场页和它的图片子元素隐藏，方便下次入场时出现“加载效果”
-        $(last).addClass("hide");
-        $(last).find("img").addClass("hide");
-        //动画结束后，将isMoving置为false
-        isMoving = false
-        //撤销动画类
-        $(now).removeClass(inClass);
-        $(last).removeClass(outClass);
-
-
-    },600)
-}
-
-
-
-
-
 
 
